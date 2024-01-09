1df235f8c5f0adc1e2c22f02eaf96cb60ba6d365
BacSiChongKhungBo <huywewe123@gmail.com>
2024-01-09 11:06:23 +0700

    bai3

---
+++ Bai3_1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Text;
+
+namespace Bai3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region nhập xuất tiếng việt trong C#
+            //nhập : cho nhập tiếng việt khi sử dụng unikey
+            Console.InputEncoding = Encoding.Unicode;
+            //xuất : cho phép hiển thị tiếng việt
+            Console.OutputEncoding = Encoding.UTF8;
+            #endregion
+
+            #region while
+            /*
+             * while (<điều kiện trả về true>)
+             * {
+             *      thực hiện khối lệnh
+             *      nếu <điều kiện trả về true>
+             *      thỏa mãn
+             * }
+             * 
+             */
+            int count = 5;
+            while (count <= 10)
+            {
+                Console.WriteLine("số: " + count);
+                count++;
+                Console.WriteLine("số sau ++: " + count);
+                Console.WriteLine("điều kiện vòng lặp sau");
+                Console.WriteLine("sẽ trả về: " + (count <= 10));
+            }
+            #endregion
+
+            #region while + switch case
+            //B1: khai báo khởi tạo giá trị cho biến choice
+            int choice = -1; // giá trị không được == giá trị thoát
+            //B2: tạo vòng lặp while với 
+            // điều kiện != lựa chọn thoát
+            while (choice != 3)
+            {
+                //B3: in ra menu và yêu cầu nhập
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Gà rán");
+                Console.WriteLine("2. Ghẹ 45kg");
+                Console.WriteLine("3. Thoát");
+                Console.WriteLine("Xin mời nhập lựa chọn: ");
+                //B4: Nhập giá trị cho choice với readline
+                choice = int.Parse(Console.ReadLine());
+                //B5: viết switch case
+                switch(choice)
+                {
+                    case 1:
+                        Console.WriteLine("chọn gà");
+                        break;
+                    case 2:
+                        Console.WriteLine("chọn ghẹ");
+                        break;
+                    case 3:
+                        Console.WriteLine("bye");
+                        break;
+                    //B6: viết default
+                    default:
+                        Console.WriteLine("xin mời nhập lại");
+                        break;
+                }
+            }
+            #endregion
+            //khai báo  4 số ax ay, bx by
+            //tạo 1 vòng lặp: (dk thoát là ax = bx, ay != by)
+            // nhập giá trị cho ax, ay, bx, by
+            // nếu ax = bx, ay != by => in ra màn hình : "bạn đang rơi tự do"
+            // ngược lại: xin mời nhập lại tọa độ
+
+            #region portal
+            int ax = 0, ay = 0, bx = 0, by = 0;
+            while(!(ax == bx && ay != by))
+            {
+                Console.WriteLine("nhập giá trị ax: ");
+                ax = int.Parse(Console.ReadLine());
+                Console.WriteLine("nhập giá trị ay: ");
+                ay = int.Parse(Console.ReadLine());
+                Console.WriteLine("nhập giá trị bx: ");
+                bx = int.Parse(Console.ReadLine());
+                Console.WriteLine("nhập giá trị by: ");
+                by = int.Parse(Console.ReadLine());
+                if (ax == bx && ay != by)
+                {
+                    Console.WriteLine("bạn đang rơi tự do");
+                }
+                else
+                {
+                    Console.WriteLine("xin mời nhập lại");
+                }
+            }
+            #endregion
+        }
+    }
+}
---
+++ Bai3_2/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Bai3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region do while
+            /* Do while cú pháp: do { enter
+             * do
+             * {
+             *      // thực hiện khối lệnh 
+             * }while(<điều kiện trả về true>)
+             */
+
+            // Khác nhau giữa while và dowhile
+            // 1. vị trí đặt while
+            //      while : đặt trên đầu
+            //      do while: đặt dưới đít
+            // 2. Vận hành
+            //      while: check dk trước, lặp sau
+            //      dowhile: chạy trước, check sau
+            int count = 11;
+            while (count < 10) 
+            {
+                Console.WriteLine("while in ra " + count);
+                count++;
+            }
+            //----------------
+            do
+            {
+                Console.WriteLine("dowhile in ra " + count);
+                count++;
+            }while (count < 10);
+            #endregion
+
+            #region do while + switch case
+            int choice; //gán giá trị trong do while là không nhất thiết
+            do
+            {
+                //B3: in ra menu và yêu cầu nhập
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Gà rán");
+                Console.WriteLine("2. Ghẹ 45kg");
+                Console.WriteLine("3. Thoát");
+                Console.WriteLine("Xin mời nhập lựa chọn: ");
+                //B4: Nhập giá trị cho choice với readline
+                choice = int.Parse(Console.ReadLine());
+                //B5: viết switch case
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("chọn gà");
+                        break;
+                    case 2:
+                        Console.WriteLine("chọn ghẹ");
+                        break;
+                    case 3:
+                        Console.WriteLine("bye");
+                        break;
+                    //B6: viết default
+                    default:
+                        Console.WriteLine("xin mời nhập lại");
+                        break;
+                }
+            } while (choice != 3);
+            #endregion
+        }
+    }
+}
