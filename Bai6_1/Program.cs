139f692113a83d6565bcee9fc981820b3f1fce3d
BacSiChongKhungBo <huywewe123@gmail.com>
2024-01-25 11:08:48 +0700

    amen

--- Bai6_1/Program.cs
+++ Bai6_1/Program.cs
@@ -18,6 +18,8 @@ namespace Bai6_1
                 Console.WriteLine("2. Xuất 1 đối tượng hoặc danh sách đối tượng");
                 Console.WriteLine("3. Xuất danh sách người cao tuổi < 70 tuổi");
                 Console.WriteLine("4. Tìm người cao tuổi theo mã");
+                Console.WriteLine("5. sắp xếp tăng dần theo tuổi");
+                Console.WriteLine("5. sắp xếp giảm dần theo tuổi");
                 Console.WriteLine("Mời bạn nhap lựa chọn: ");
                 chon = int.Parse(Console.ReadLine());
                 switch (chon)
@@ -26,13 +28,16 @@ namespace Bai6_1
                         _qlyNCT.ThemNCT();
                         break;
                     case 2:
-
+                        _qlyNCT.XuatDanhSach();
                         break;
                     case 3:
 
                         break;
-                    case 4:
-
+                    case 5:
+                        _qlyNCT.SapXepDSLinq();
+                        break;
+                    case 6:
+                        _qlyNCT.SapXepDSLinqDESC();
                         break;
                     default:
                         break;
--- Bai6_1/QLNguoiCT.cs
+++ Bai6_1/QLNguoiCT.cs
@@ -19,7 +19,7 @@ namespace Bai6_1
         // và khởi tạo danh sách đã khai báo
         public QLNguoiCT()
         {
-            lst_nct = new List<NguoiCaoTuoi> ();
+            lst_nct = new List<NguoiCaoTuoi>();
         }
 
         #region Thêm đối tượng
@@ -45,11 +45,11 @@ namespace Bai6_1
             Console.WriteLine("bạn có muốn nhập tiếp ko?");
             Console.WriteLine("Y/N?");
             choice = Console.ReadLine();
-            if(choice == "Y") 
+            if (choice == "Y")
             {
                 ThemNCT();
             }
-            
+
         }
         #endregion
 
@@ -75,8 +75,8 @@ namespace Bai6_1
             //B2: tìm mã
             //C1: Linq
             NguoiCaoTuoi nguoiCanXoa;
-            nguoiCanXoa  = lst_nct.Where(x => x.Ma.Equals(input)).FirstOrDefault();
-            if(nguoiCanXoa != null)
+            nguoiCanXoa = lst_nct.Where(x => x.Ma.Equals(input)).FirstOrDefault();
+            if (nguoiCanXoa != null)
             {
                 lst_nct.Remove(nguoiCanXoa);
             }
@@ -110,6 +110,244 @@ namespace Bai6_1
             //}
         }
         #endregion
+
+        #region Sửa
+        public void SuaThongTinTheoMa()
+        {
+            bool timthay = false;
+            //B1: nhập mã để kiểm tra
+            Console.WriteLine("Xin mời nhập mã cần sửa");
+            string input = Console.ReadLine();
+            //B2: Chạy foreach tìm người trùng mã và sửa
+            foreach (var nguoi in lst_nct)
+            {
+                if (nguoi.Ma == input)
+                {
+                    Console.WriteLine("xin mời nhập mã mới");
+                    nguoi.Ma = Console.ReadLine();
+                    Console.WriteLine("Xin mời nhập tên mới");
+                    nguoi.Ten = Console.ReadLine();
+                    Console.WriteLine("Xin mời nhập tuổi mới");
+                    nguoi.Tuoi = int.Parse(Console.ReadLine());
+                    timthay = true;
+                    break;
+                }
+            }
+            if (timthay) // tim thấy = true
+            {
+                Console.WriteLine("Sửa thành công");
+            }
+            else
+            {
+                Console.WriteLine("Sửa thất bại");
+            }
+        }
+        #endregion 
+        #region Sắp xếp tăng dần
+        public void SapXepTangDanBth()
+        {
+            //B1:tạo 1 danh sách tạm
+            List<NguoiCaoTuoi> sorted;
+            sorted = lst_nct;
+            //B2: sắp xếp
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                for (int j = i + 1; j < sorted.Count; j++)
+                {
+                    if (sorted[i].Tuoi > sorted[j].Tuoi)
+                    {
+                        int tmp = sorted[i].Tuoi;
+                        sorted[i].Tuoi = sorted[j].Tuoi;
+                        sorted[j].Tuoi = tmp;
+                    }
+                }
+            }
+            Console.WriteLine("kết quả");
+            foreach (var item in lst_nct)
+            {
+                item.InThongTin();
+            }
+        }
+
+        public void SapXepDSLinq()
+        {
+            //B1:tạo 1 danh sách tạm
+            List<NguoiCaoTuoi> sorted;
+            //B2: sắp xếp = linq
+            sorted = lst_nct.OrderBy(nct => nct.Tuoi).ToList();
+            //Dịch linq:
+            // danh sách người cao tuổi: lst_nct.
+            // được sx tăng dần theo tuổi của nct:  OrderBy(nct => nct.Tuoi)
+            // và chuyển thành danh sách : ToList()
+            //B3:In ra kết quả
+            Console.WriteLine("kết quả");
+            foreach (var item in lst_nct)
+            {
+                item.InThongTin();
+            }
+        }
+        #endregion
+        #region Sắp xếp giảm dần
+        public void SapXepDSLinqDESC()
+        {
+            List<NguoiCaoTuoi> sorted = lst_nct.OrderByDescending(nct => nct.Tuoi).ToList();
+            foreach (var item in lst_nct)
+            {
+                item.InThongTin();
+            }
+        }
+        #endregion
+        #region Tìm kiếm theo mã
+        // Chú ý: mã thường là duy nhất nên
+        // chỉ cần tạo một đối tượng kết quả
+        public void TimTheoMa()
+        {
+            //B1: chuẩn bị input
+            string input;
+            Console.WriteLine("Xin mời nhập mã");
+            input = Console.ReadLine();
+            NguoiCaoTuoi result;
+            result = lst_nct.Where(nct => nct.Ma == input).FirstOrDefault();
+            // tìm trong danh sách : lst_nct.
+            // where(nct => nct.Ma : mà mã người cao tuổi
+            // == input : bằng với mã nhập từ bàn phím
+            // firstordefault: lấy người đầu tiên hoặc null
+            if (result == null)
+            {
+                Console.WriteLine("Không thấy");
+            }
+            else
+            {
+                result.InThongTin();
+            }
+        }
+        #endregion
+
+        #region tìm kiếm theo các thuộc tính bình thường
+        // với các thuộc tính bình thường,
+        // thay vì tạo đối tượng result
+        // thì tạo danh sách đối tượng
+        public void TimTheoTuoi()
+        {
+            //tìm những người có tuổi > 60
+            List<NguoiCaoTuoi> results; //danh sách kết quả
+            Console.WriteLine("Bạn muốn tìm người > bao nhiêu tuổi");
+            int input = int.Parse(Console.ReadLine());
+            //linq
+            results = lst_nct.Where(nct => nct.Tuoi > input).ToList();
+            foreach (var nguoi in results)
+            {
+                nguoi.InThongTin();
+            }
+        }
+
+        public void TimTheoKhoangTuoi()
+        {
+            //tìm những người có tuổi > 60  và < 90
+            List<NguoiCaoTuoi> results; //danh sách kết quả
+            Console.WriteLine("Bạn muốn tìm người từ bao nhiêu tuổi");
+            int inputmin = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bạn muốn tìm người đén bao nhiêu tuổi");
+            int inputmax = int.Parse(Console.ReadLine());
+            //linq
+            results = lst_nct.Where(nct => nct.Tuoi > inputmin
+                                 && nct.Tuoi < inputmax).ToList();
+            foreach (var nguoi in results)
+            {
+                nguoi.InThongTin();
+            }
+        }
+
+        //Tìm tên bắt đầu bằng ?
+        public void TimTheoTen()
+        {
+            List<NguoiCaoTuoi> results;
+            Console.WriteLine("Xin mời nhập họ");
+            string input = Console.ReadLine();
+            results = lst_nct.Where(nct => nct.Ten.StartsWith(input)).ToList();
+            if (results.Count == 0)
+            {
+                Console.WriteLine("không tìm thấy");
+                return; // kết thúc hàm mà không chạy bất kì dòng code bên dưới
+            }
+            foreach (var nguoi in results)
+            {
+                nguoi.InThongTin();
+            }
+        }
+        public void TimTheoTen1() // tìm tên đệm || tìm theo tên chứa chữ cái
+        {
+            List<NguoiCaoTuoi> results;
+            Console.WriteLine("Xin mời nhập họ");
+            string input = Console.ReadLine();
+            results = lst_nct.Where(nct => nct.Ten.Contains(input)).ToList();
+            if (results.Count == 0)
+            {
+                Console.WriteLine("không tìm thấy");
+                return; // kết thúc hàm mà không chạy bất kì dòng code bên dưới
+            }
+            foreach (var nguoi in results)
+            {
+                nguoi.InThongTin();
+            }
+        }
+
+        public void TimTheoTen2() // tìm tên xưng hô || chữ cái cuối cùng của tên
+        {
+            List<NguoiCaoTuoi> results;
+            Console.WriteLine("Xin mời nhập họ");
+            string input = Console.ReadLine();
+            results = lst_nct.Where(nct => nct.Ten.EndsWith(input)).ToList();
+            if (results.Count == 0)
+            {
+                Console.WriteLine("không tìm thấy");
+                return; // kết thúc hàm mà không chạy bất kì dòng code bên dưới
+            }
+            foreach (var nguoi in results)
+            {
+                nguoi.InThongTin();
+            }
+        }
+
+        public void TimTheoTen3()
+        {
+            // không hiển thị họ mà không muốn thấy
+            List<NguoiCaoTuoi> results;
+            Console.WriteLine("Xin mời nhập họ");
+            string input = Console.ReadLine();
+            results = lst_nct.Where(nct => !nct.Ten.StartsWith(input)).ToList();
+            if (results.Count == 0)
+            {
+                Console.WriteLine("không tìm thấy");
+                return; // kết thúc hàm mà không chạy bất kì dòng code bên dưới
+            }
+            foreach (var nguoi in results)
+            {
+                nguoi.InThongTin();
+            }
+        }
+
+        public void TimKetHop()
+        {
+            // tìm theo ten va tuoi
+            List<NguoiCaoTuoi> results;
+            Console.WriteLine("Xin mời nhập tên");
+            string input = Console.ReadLine();
+            Console.WriteLine("Xin mời nhập tuoi");
+            int input2 = int.Parse(Console.ReadLine());
+            results = lst_nct.Where(nct => nct.Ten.Contains(input) && nct.Tuoi == input2).ToList();
+            if (results.Count == 0)
+            {
+                Console.WriteLine("không tìm thấy");
+                return; // kết thúc hàm mà không chạy bất kì dòng code bên dưới
+            }
+            foreach (var nguoi in results)
+            {
+                nguoi.InThongTin();
+            }
+        }
+        #endregion
+
 
     }
 }
